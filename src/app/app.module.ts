5b0e704fad7be547786bd083c8c1f8861f831d42
黃升煌 Mike <mike@miniasp.com>
2020-10-05 11:21:03 +0800

    練習將畫面拆成多個元件組合

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { ArticleComponent } from './article/article.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    ArticleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
